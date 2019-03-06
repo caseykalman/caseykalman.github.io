2b27b747e1935a5b365874a63ccf17665b39f04a
Casey <kalman.casey@gmail.com>
2019-03-06 14:04:41 -0500

    Adding some interactives
    No yet linked on the homepage.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready( function( $ ) {
+
+$(window).load(function(){
+            
+    $(".twentytwenty-container[data-orientation!=\'vertical\']").twentytwenty({
+      default_offset_pct:0.5,
+      no_overlay:false,
+      before_label:2005,
+      after_label:2006 });
+    $(".twentytwenty-before-label").html("");
+    $(".twentytwenty-after-label").html("");
+ });
+} );
